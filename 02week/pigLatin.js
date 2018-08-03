19a560861e1c20bb4499505f2dfe3876515e4529
Christelle <hopeandash@gmail.com>
2018-08-02 19:22:08 -0500

    firstOlympics

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,7 +10,103 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+//  PIG LATIN
+// writting a Game where the user inputs a value that contains letters where if the word begins with
+// 1 consonant that letter is moved to the end of the word and the letter ay( the string "ay") is added to the actual letter such as pig would be igpay
+// if a word begins with two consonants both letters are moved to the end and the string ay is appended to the actual word example grade adegray
+// if a word begins with a vowel the letter remains in place and the srting way is appended to the actual word example apple appleway
+
+// The users input is valid when they write words with the 26 letters of the alphabet.
+// the users input will the be tranformed into an array (the array process is used to detached the letters and moved them)
+// we will be looping thru the words and removing letter till they begin with a voyel
+// the split and join methods are used to split and join the elements of an array
+// the voyels are [a,e,i,o,u,y]
+// const voyels= ['a','e','i','o','y']
+// // (!voyels) would be for my consonants
+
+// write a function that takes the valid input that contains only letters blocks out numbers and other strange chararcter
+
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+const letters = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+];
+
+const mainMethod =(word) => {
+  let l = lettersOnly(word);
+  if(l === false)
+  {
+    return "Please Enter letters Only";
+  } 
+  return latinizedWord(word);
+
+}
+const latinizedWord =(word)=>{
+  //  let wordArr = word.split("");
+  let firstSection = "";
+  let secondSection = "";
+
+  if(vowels.indexOf(word[0]) < 0) //the word starts with a consonant do this 
+  {
+    for (let i = 0; i < word.length; i++) {
+		  if (vowels.indexOf(word[i]) > -1) {
+			
+        firstSection = word.substring(0,i)
+        secondSection = word.substring(i)
+        break;
+      
+	  	}
+    }
+  
+    return secondSection+firstSection+ "ay"
+  } else{ 
+  return word + "way"
+  }
+}
+const lettersOnly = (word) => {
+	let letterChecker = 0;
+
+	for (let i = 0; i < word.length; i++) {
+		if (letters.indexOf(word[i]) > -1) {
+			letterChecker = letterChecker + 1;
+      
+		}
+  }
+    
+		if (letterChecker === word.length) {
+			return true;
+		} else {
+			return false;
+		}
+	
+};
+
+
+mainMethod("eloquent"); // Your code here
 
 }
 
---
+++ gitOlympics.js
@@ -0,0 +1,25 @@
+'use strict'
+// Teams of 2/3
+// - Each team member will
+// create a branch off of gh-pages called gitOlympics
+// create a new file called gitOlympics.js in 04week/
+// in gitOlympics.js
+// ‘use strict’ at the top
+// create a function called printListOfOlympians 
+// the function should accept an array and use a forEach to console.log() every item in the array
+// call the function with a test array of at least 5 names
+// Commit at least 3 times
+// Push to github, create a pull request
+// the PR must be clean, the only file changed should be gitOlympics.js
+// When everyone has an open pull request raise your hand for a check
+// The first team with all pull requests merged/closed - wins
+
+
+const printListOfOlympians =(inputArr)=> {
+  inputArr.forEach =((item)=>{
+    console.log (item);
+  });
+
+}
+const names =["michaelPhelps", "yousinVualt","muhamedAli","Zeus","Jake"]
+printListOfOlympias (names);
