8284cbbf60295b617a5d79967f4369819cbfaa26
Christelle <hopeanddash@gmail.com>
2018-08-08 09:11:42 -0500

    not again

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,4 +1,4 @@
-5'use strict';
+'use strict';
 
 const assert = require('assert');
 const readline = require('readline');
@@ -6,7 +6,8 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+//  in this Object called stacks the rows a, b ,c represent the 3 holding block for our elements to move
+// in this case our elements are in order from top to bottom 1 thru 4 1 being the least 4 being the greatest
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
@@ -19,15 +20,20 @@ const printStacks=()=> {
   console.log("c: " + stacks.c);
 }
 
+
+// the move piece allows the elements from 1 to 4 to be moved from stack to stack the startstack being the block where teh element is taken and the end stack being the block where the element is added
 const movePiece=(startStack, endStack)=> {
   return stacks[endStack].push(stacks[startStack].pop());node
 
 }
 // conditionals 
+// in this case a move is only legal if the lowest number is stacked on top of a bigger number 
 const isLegal=(startStack,endStack)=> {
   if(valueOfStack (startStack,endStack)) {
+    // the .lenght -1 function alwasy targets the last element of the array in the case the stack
   const firstValue = stacks[startStack][stacks[startStack].length-1]
     const secondValue = stacks[endStack][stacks[endStack].length-1]
+    // this allows the move to be legal after the comparion of the value
       if(firstValue < secondValue || stacks[endStack].length===0){
        return true
      }else{
@@ -35,8 +41,9 @@ const isLegal=(startStack,endStack)=> {
     }
 }
 }
-
+//  there are 3 stack here are all the possible outcomes 
 const valueOfStack =(startStack,endStack)=>{
+  // if a is considere the start stack b and c become the obvious endstack where we no place the input at hand same for b and c
 if (startStack ==='a'&& endStack==="b"|| endStack ==="c"){
   return true;
 }else if( startStack ==="b" && endStack==="a"|| endStack==="c"){
@@ -49,7 +56,7 @@ return true;
 }
 
 
-
+// the check for win functions alerst us when all the 4 items are placed on the stack 
 const checkForWin=()=> {
   if (stacks.c.length === 4){
     return true;
@@ -58,7 +65,8 @@ const checkForWin=()=> {
   }
 }
 
-// parent function Main Function where the game happens
+// Parent function Main Function where the game happens
+// if the move is legal then we move all the pieces then it's a win the treminal announces that the game is won.
 
 const towersOfHanoi=(startStack, endStack) => {
   if (isLegal(startStack,endStack)){
@@ -68,6 +76,7 @@ const towersOfHanoi=(startStack, endStack) => {
   } 
   if (checkForWin()){
     console.log (" YOU ARE A GENIUS YOU WON!!!")
+    // this resets the game back to it's original structure which in  this case was the 4 elements on the first stack...
     stacks = {
       a: [4, 3, 2, 1],
       b: [],
